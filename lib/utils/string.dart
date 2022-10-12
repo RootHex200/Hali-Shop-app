6c4aeecadb805c15bac68f6c59eb00a2e9ff70a5
Anwar-Kabir <munnarahman1994@gmail.com>
2022-10-13 00:46:00 +0600

    app luncher icon added

--- lib/utils/string.dart
+++ lib/utils/string.dart
@@ -1,5 +1,81 @@
+class AppString {
+  //login page text
+  static const String login = "Login";
+  static const String enterYourMobileNumber = "Enter Your Mobile Number";
+  static const String countryCode = "+88";
+  static const String loginWithOTP = "Login with OTP";
+  static const String or = "or";
+  static const String loginWithUserID = "Login with User ID";
+  static const String dontHaveAnAccount = "Don't have an account ?";
+  static const String creatAnAccount = "Create an account";
+  static const String moreSettings = "More settings";
 
+  ///sign up page
+  static const String createAnAccount = "Creat an Account";
+  static const String alreadyRegistered = "Already Registered";
 
-class AppString{
-  // Monarch mart Apps Strings
+  ///profile page text
+  static const String basicInformation = "Basic Information";
+  static const String name = "Name";
+  static const String password = "Password";
+  static const String passConform = "Confirm Password";
+  static const String updateProfile = "Update Profile";
+  static const String passwordhelper = "Password must be at least 6 character ";
+  static const String editprofiletitle = "Edit Profile";
+
+  ///logout
+  static const String logout = "Log Out";
+  static const String logoutAlart = "Do you really want to logout?";
+
+  ///shipping Adress
+  static const String noadress = "No address available";
+
+  ///URL webview
+  static const String url = "https://pub.dev/packages/url_launcher/example";
+
+  ///Add user address
+  static const String nameAdress = "Name *";
+  static const String dateOfBirth = "Date of Birth";
+  static const String gender = "Gender *";
+  static const String gendervalu = "Select Gender";
+  static const String address = "Address *";
+  static const String postal = "Postal *";
+  static const String city = "City *";
+  static const String phoneadress = "Phone *";
+
+  ///Home page
+  static const String topSellers = "Top Sellers";
+  static const String topBrands = "Top Brands";
+  static const String bestSelling = "Best Selling";
+  static const String newArrival = "New Arrival";
+  static const String alloffers = "All offers";
+  static const String topCategories = "Top Categories";
+  static const String featuredProducts = "Featured Products";
+  static const String onSaleProducts = "On Sale Products";
+  static const String fashion = "Fashion";
+  static const String electronicsAppliance = "Electronics And Appliance";
+  static const String viewAll = "View All";
+
+  /// add to card text
+  static const String totalAmount = "Total Amount";
+  static const String proceedToOrder = "Proceed To Order";
+
+  ///navigation  drawer
+  static const String myAccount = "My Account";
+  static const String privacyPolicy = "Privacy Policy";
+  static const String returnPolicy = "Return Policy";
+  static const String terms = "Terms";
+  static const String drawerLogout = "Logout";
+  static const String hellosignIn = "Hello Sign in";
+
+  ///update needed, date - October 2, 2022
+  static const String updateNeeded = "UPDATE NEEDED!";
+  static const String updatdContanted = "Please updated your Monarch Mart App";
+  static const String updateMonarchMart = "UPDATE";
+
+  ///checkout
+  static const String checkOutwithbkash = "Checkout with Bkash";
+  static const String checkOutwithNagad = "Checkout with Nagad";
+  static const String checkOutwithUpay = "Checkout with Upay";
+  static const String cashonDelivery = "Cash on delivery";
 }
